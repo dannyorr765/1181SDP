b3654ef841455bed74d417779d7e4e8105ac9059
dannyorr765 <dannyorr765@yahoo.com>
2025-12-01 21:40:01 -0500

    Delete welcomeScene.m

--- welcomeScene.m
+++
@@ -1,7 +0,0 @@
-clc
-[spriteList] = getSprites();
-[textureList] = getTextures(spriteList);
-entryScene = Scene([0,0,0], spriteList, 64,36);
-% need to make box where they can enter number of teams
-entryScene.fillRectHollow(21,24,29,40, textureList{1},[16,36,181])
-entryScene.renderScene();
