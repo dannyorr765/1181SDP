6bb1fdbc1ca3ba2e37a2dee964cfce4d1cc9f63b
dannyorr765 <dannyorr765@yahoo.com>
2025-12-01 21:39:55 -0500

    Delete test.m

--- test.m
+++
@@ -1,7 +0,0 @@
-[spriteList] = getSprites();
-[textureList] = getTextures(spriteList);
-testScene = Scene([0,0,0], spriteList, 20, 30);
-
-testScene.fillRect(1, 1, 3, 3, textureList{1}, [0,0,255]);
-testScene.insertText(10, 1, "Poopy butt", [255,0,0]);
-testScene.renderScene();
